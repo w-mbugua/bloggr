17725d230253142185899fb9f93a19ad0b372400
JoyMbugua <wacumbugua@gmail.com>
2021-05-01 13:45:01 +0300

    created individual post view and template

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 from . import main
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, abort
 from flask_login import login_required
 from .forms import BlogPost
 from .. import db
@@ -24,3 +24,10 @@ def new_blog():
         return redirect(url_for('main.index'))
     flash("Check your input!", 'warning')
     return render_template('new_blog.html', form = form)
+
+@main.route('/blog/<int:id>')
+def read_blog(id):
+    blog = Blog.query.get_or_404(id)
+    if blog is None:
+        abort(404)
+    return render_template('blog.html', blog = blog)
