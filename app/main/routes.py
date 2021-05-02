ac0bf11cef09442803e47ca964f8d8241d20b29a
JoyMbugua <wacumbugua@gmail.com>
2021-05-02 07:54:14 +0300

    added a quotes function

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,12 +4,14 @@ from flask_login import login_required, current_user
 from .forms import BlogPost, CommentForm, UpdateProfile
 from .. import db
 from ..models import Blog, Comment, Writer
+import urllib.request, json
 
 
 @main.route('/')
 def index():
-    blogposts = Blog.query.all()
-    return render_template('index.html', title = 'Home Page', posts = blogposts)
+    blogposts = Blog.query.order_by(Blog.date_posted.desc()).all()
+    quote = random_quote()
+    return render_template('index.html', title = 'Home Page', posts = blogposts, quote = quote)
 
 @main.route('/blog/new', methods = ['GET', 'POST'])
 @login_required
@@ -36,7 +38,6 @@ def read_blog(id):
     return render_template('blog.html', blog = blog, form = comment_form, comments = comments)
 
 @main.route('/comment/<int:id>', methods = ['GET','POST'])
-@login_required
 def comment(id):
    blog = Blog.query.get_or_404(id)
    form = CommentForm()
@@ -88,6 +89,18 @@ def blogupdate(id):
    form.body.data = blog.post
    form.title.data = blog.title 
    return render_template('new_blog.html', form = form)
+
+def random_quote():
+    base_url = 'http://quotes.stormconsultancy.co.uk/random.json'
+    with urllib.request.urlopen(base_url) as url:
+        data = url.read()
+        data = json.loads(data)
+    
+    quote = None
+
+    if data['quote']:
+        quote = data
+    return quote
 
 
  
